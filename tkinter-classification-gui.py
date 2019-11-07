299ae09f8bd2b3f1bee8d30d5b4beb0af07771c2
lfzbrennan <55155911+lfzbrennan@users.noreply.github.com>
2019-11-07 15:52:12 -0500

    new version

--- tkinter-classification-gui.py
+++ tkinter-classification-gui.py
@@ -8,73 +8,67 @@ import argparse
 
 
 class CreateDisplay:
-    def __init__(self, master, save_dir, images, current_image, class_one="good", class_two="bad", mode="move"):
+    def __init__(self, master, save_dir, images, current_image, classes="good", mode="save"):
         self.master = master
         self.count = 0
         self.save_dir = save_dir
         self.current_image = current_image
         self.images = images
         self.mode = mode
-        self.class1 = class_one
-        self.class2 = class_two
+        self.classes = classes
         master.title("Classify Image")
 
         self.display_image()
 
-        self.button_1 = Button(self.master, text=class_one, command=self.change_image_good)
-        self.button_1.grid(row=1, column=0)
+        self.col = len(self.classes)
 
-        self.button_2 = Button(self.master, text=class_two, command=self.change_image_bad)
-        self.button_2.grid(row=1, column=1)
+        buttons = []
 
-    def change_image_good(self):
+        for i in range(len(self.classes)):
+            buttons.append(Button(self.master, text=self.classes[i], command=lambda: self.change_image(self.classes[i]))).grid(row=0, columnspan=i)
+
+    def change_image(self, class_):
+
         self.count += 1
 
         path = self.current_image.split("/")[-1]
 
         if self.mode == "move":
-            os.rename(self.current_image, self.save_dir + "/" + self.class1 + "/" + path)
+            os.rename(self.current_image, self.save_dir + "/" + class_ + "/" + path)
+        elif self.mode == "copy":
+            copyfile(self.current_image, self.save_dir + "/" + class_ + "/" + path)
+
         else:
-            copyfile(self.current_image, self.save_dir + "/" + self.class1 + "/" + path)
+            raise ValueError('Mode must be set to "move" or "copy"')
 
         self.current_image = self.images[self.count]
 
         self.display_image()
 
-    def change_image_bad(self):
-        self.count += 1
-
-        path = self.current_image.split("/")[-1]
-
-        if self.mode == "move":
-            os.rename(self.current_image, self.save_dir + "/" + self.class2 + "/" + path)
-        else:
-            copyfile(self.current_image, self.save_dir + "/" + self.class2 + "/" + path)
-
-        self.current_image = self.images[self.count]
-
-        self.display_image()
 
     def display_image(self):
         img = Image.open(self.current_image)
         img = img.resize((512, 512))
         self.img = ImageTk.PhotoImage(img)
         self.image = Label(self.master, image=self.img)
-        self.image.grid(row=0, columnspan=2)
+        self.image.grid(row=0, columnspan=self.col)
 
 
 def main(args):
     parser = argparse.ArgumentParser(description='Save cascading images from directory')
     parser.add_argument('--root_dir', default='in', type=str, help='where unclassified dataset is stored')
     parser.add_argument('--save_dir', default='out', type=str, help='where classified data is stored')
-    parser.add_argument('--class1', default='good', type=str, help='classification 1')
-    parser.add_argument('--class2', default='bad', type=str, help='classification 2')
+    parser.add_argument('--classes', nargs="+", default='uno', type=str, help='list of classses')
     parser.add_argument('--mode', default='move', type=str, help='"move" or "copy": "move" moves the files (deleting them from the root directory), where "copy" just makes a copy (keeps root)')
 
     args = parser.parse_args()
 
-    save_dir = args.save_dir
-    root_dir = args.root_dir
+    #save_dir = args.save_dir
+    #root_dir = args.root_dir
+
+    # root dir, save dir, class 1, class 2, mode (copy or mv)
+    root_dir = r"/Volumes/Seagate Backup Plus Drive/mf_img/sorted_images/*/*"
+    save_dir = r"/Volumes/Seagate Backup Plus Drive/mf_img/good_or_bad/"
 
     print("Globbing")
 
@@ -93,7 +87,8 @@ def main(args):
     print("Creating GUI")
 
     root = Tk()
-    CreateDisplay(root, save_dir, all_images, CURR_IMG, class_one=args.class1, class_two=args.class2, mode=args.mode)
+    CreateDisplay(root, save_dir, all_images, CURR_IMG)
+    #CreateDisplay(root, save_dir, all_images, CURR_IMG, class_one=args.class1, class_two=args.class2, mode=args.mode)
     root.mainloop()
 
 
@@ -105,6 +100,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         pass
     sys.exit()
-
-
 
